8018bbd047f0559f436e79dbbee4fd837049910f
utkazu <hiroshimazeal@gmail.com>
2020-04-14 17:58:25 +0900

    solve n+1 problem

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,12 @@
 class StudentsController < ApplicationController
   def index
     @q = Student.ransack(params[:q])
-    @students = @q.result(distinct: true)
+    @students = @q.result.includes(:department, :subjects)
   end
 
   def search
     @q = Student.serch(search_params)
-    @students = @q.result(distinct: true)
+    @students = @q.result.includes(:department, :subjects)
   end
 
   private
