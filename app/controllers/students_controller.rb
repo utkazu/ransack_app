36455333ab8cbd6b27efcca9bb9a954ee201595c
utkazu <hiroshimazeal@gmail.com>
2020-04-14 20:18:24 +0900

    inplement search with select box

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,17 @@
 class StudentsController < ApplicationController
   def index
     @q = Student.ransack(params[:q])
+    @departments = Department.all
     @students = @q.result.includes(:department, :subjects)
   end
 
   def search
-    @q = Student.search(search_params)
+    @q = Student.ransack(search_params)
     @students = @q.result.includes(:department, :subjects)
   end
 
   private
   def search_params
-    params.require(:q).permit(:name_cont)
+    params.require(:q).permit(:department_id_eq)
   end
 end
